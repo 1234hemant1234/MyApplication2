dea53ebd63aea7a3fdd8ab31211de8e93ffbf157
hemantmalhotra <hemant.malhotra_mca18@gla.ac.in>
2019-02-21 11:45:45 +0530

    action change 4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "hemant malhotra 2nd year mca ", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "hemant malhotra 2nd year mca 188429020", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
